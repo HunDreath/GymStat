026867254dd0f9d3287d495e76403a311ae3337b
HunDreath <lucas_morin@icloud.com>
2025-10-08 21:13:53 +0200

    Initial Commit

---
+++ GymStat/GymStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymStatApp.swift
+//  GymStat
+//
+//  Created by Lucas Morin on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct GymStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
